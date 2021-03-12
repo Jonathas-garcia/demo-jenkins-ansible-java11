f7c8faa4052d5890305d2c2913a72e095727d6e4
Jonathas Garcia <jonathas-garcia>
2021-03-12 18:00:16 -0300

    First commit

---
+++ src/main/java/br/com/jonathas/demojenkinsansiblejava11/DemoJenkinsAnsibleJava11Application.java
@@ -0,0 +1,13 @@
+package br.com.jonathas.demojenkinsansiblejava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsAnsibleJava11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkinsAnsibleJava11Application.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/jonathas/demojenkinsansiblejava11/controller/Controller.java
@@ -0,0 +1,16 @@
+package br.com.jonathas.demojenkinsansiblejava11.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("teste")
+public class Controller {
+
+    @GetMapping
+    public String demo() {
+        return "OK JAVA 11";
+    }
+
+}
